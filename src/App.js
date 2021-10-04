fae8ad35d1b5687af36f912d0fbefb5f1c982adb
unknown <zmac7713@gmail.com>
2021-10-04 17:45:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+     <h1> 
+        Don't just integrate Accelerate your CRM
+     </h1> 
+     <p>Propel your team into a state of flow with CRM Accelerations. Switch
+       out boring hours
+       of repetitive work with high-impact activities. Build a repeatable 
+       sales process.
+     </p>
+     <div classname="get1">
+       <button1 classname="Get1">GET STARTED</button1>
+      </div>
+      <div classname="get2">
+      <button2 classname="get Get 2">Try For Free</button2>
+      </div>
+      <div classname="log">
+      <onclick classname="log1">Log in</onclick>
+      </div>
+      <div >
+        <h2>Engage instantly with prospects who show intent</h2>
+        <p2>Auto-sync email engagement data like opens, clicks and replies from Klenty right into your CRM. Get notified immediately. Followup without skipping a beat.</p2>
+      </div>
+      <div>
+        <h2 id='he'>Outreach without leaving your CRM </h2>
+        <p3>Engage with prospects straight from your CRM.Book meetings using Klenty- without ever logging into Klenty. No more switching screens to execute sales outreach.</p3>
+      </div>
+      <div classname="lvl3">
+        <h3>What our customers have to say</h3>
+      </div>
+      <div classname="lvl4">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Klenty seamlessly integrates with Pipedrive and offers straightforward email tracking and cadence management. The contact sync is simple and the support is top-notch. A very efficient and effective platform.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h5>Teresa Van Oerle Director of Business Development</h5>
+      </div>
+     
     </div>
   );
 }
